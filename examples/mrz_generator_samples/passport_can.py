2088667ce8ac6dd8c03f6cf7dea690d3c3aa7ca7
Mathewmerlin <153445566+Mathewmerlin@users.noreply.github.com>
2024-06-17 13:49:12 +0100

    JHON FERNANDO

--- examples/mrz_generator_samples/passport_can.py
+++ examples/mrz_generator_samples/passport_can.py
@@ -15,12 +15,12 @@ from mrz.generator.td3 import *
 
 td3_generator = TD3CodeGenerator("P",            # Document type   Normally 'P' for passport
                                  "GB",           # Country         3 letters code or country name
-                                 "MARTIN",       # Surname(s)      Special characters will be transliterated
-                                 "SARAH",        # Given name(s)   Special characters will be transliterated
+                                 "JHON",       # Surname(s)      Special characters will be transliterated
+                                 "FERNANDO",        # Given name(s)   Special characters will be transliterated
                                  "980XG47",      # Passport number
                                  "GBR",          # Nationality     3 letter code or country name
-                                 "850101",       # Birth date      YYMMDD
-                                 "F",            # Genre           Male: 'M', Female: 'F' or Undefined 'X'
+                                 "760316",       # Birth date      YYMMDD
+                                 "M",            # Genre           Male: 'M', Female: 'F' or Undefined 'X'
                                  "261228",       # Expiry date     YYMMDD
                                  "ID88933477")   # Id number       Not mandatory field
 
@@ -30,7 +30,7 @@ print("\n")
 td3_generator.country_code = "CAN"
 td3_generator.document_number = "ZE000509"
 td3_generator.nationality = "CAN"
-td3_generator.expiry_date = "230114"
+td3_generator.expiry_date = "230124"
 td3_generator.optional_data = ""
 
 print(td3_generator)
